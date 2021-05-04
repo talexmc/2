2f3dd9c061af5933f9630691479dbebce6691d90
talexmc <83471856+talexmc@users.noreply.github.com>
2021-05-03 18:36:49 -0600

    Delete main.js

--- main.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('show');
-});                                                                   
